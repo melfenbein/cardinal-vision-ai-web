376ff6e9c59db0cf98276ed3f5a83f5cfee2cb6d
melfenbein <elfmark@gmail.com>
2026-04-06 11:19:04 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+
 export const metadata = {
-  title: 'Cardinal Vision AI',
-  description: 'The decision layer for collectibles'
+  title: "Cardinal Vision AI",
+  description: "The decision layer for collectibles",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
